6f214803536e8710b904759eccb90f1b190c7de2
bhtibrewal <42600164+bhtibrewal@users.noreply.github.com>
2022-03-24 22:31:19 +0530

    fix searchbar

--- src/components/navbar/components/Search.jsx
+++ src/components/navbar/components/Search.jsx
@@ -1,6 +1,6 @@
 export const Search = () => {
     return (
-      <div className="artsy-input icon-input">
+      <div className="artsy-input icon-input search-input">
         <i className="fas fa-search"></i>
         <input placeholder="Search..." type="text" />
       </div>
